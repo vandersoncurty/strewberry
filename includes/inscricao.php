b54c9ac0086bb5b0b8e3ae096bbe828d645c5bc6
Vanderson Curty <vandincc33@gmail.com>
2023-11-21 22:01:23 -0300

    Adicionado formulário de inscrição do concurso

---
+++ includes/inscricao.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/main.css'>
+    <script src='../js/main.js'></script>
+</head>
+<body>
+    <form method="POST" action="../db/add_inscricao.php" name="registration">
+        <label for="name">Nome completo:</label>
+        <input type="text" name="name" required>
+
+        <label for="artistic_name">Nome artístico:</label>
+        <input type="text" name="artistic_name">
+
+        <label for="character_name">Nome do personagem:</label>
+        <input type="text" name="character_name" required>
+
+        <label for="character_source">Fonte do personagem:</label>
+        <input type="text" name="character_source" required>
+
+        <label for="image">Imagem de referência:</label>
+        <input type="file" name="image" required>
+
+        <label for="audio">Áudio da apresentação:</label>
+        <input type="file" name="audio" required>
+
+        <input type="submit" value="Confirmar inscrição"
+    </form>
+</body>
+</html>
