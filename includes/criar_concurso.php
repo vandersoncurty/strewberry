1986f05187eb4f52c7bb8ace8ee5f8a1ca0c284d
Vanderson Curty <vandincc33@gmail.com>
2023-11-26 19:41:41 -0300

    css

--- includes/criar_concurso.php
+++ includes/criar_concurso.php
@@ -16,14 +16,14 @@
         <!--<label for="event_date">Data do concurso:</label>
         <input type="date" name="event_date">-->
 
-        <label for="final_date">Encerramento das inscrições:</label>
+        <label for="final_date">Fim das inscrições:</label>
         <input type="datetime" name="final_date">
 
         <!--<label for="max_competitors">Número máximo de participantes:</label>
         <input type="number" name="max_competitors">-->
         
-        <label for="categories">Encerramento das inscrições:</label>
-        <input type="varchar" name="categories">
+        <label for="categories">categorias:</label>
+        <input type="checkbox" name="categories">
 
         <h2>Staff:</h2>
         <div id="staff_container">
