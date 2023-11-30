c138533d96fd914b82a6881079554a226141bc2b
Vanderson Curty <vandincc33@gmail.com>
2023-11-29 22:50:22 -0300

    resolvido bugs da tela do jurado

--- includes/tela_avaliacao.php
+++ includes/tela_avaliacao.php
@@ -1,26 +1,31 @@
 <?php
 include("../utils/functions.php");
 include("../db/conn.php");
+
 $concurso_id = $_GET['concurso_id'];
 $participante_id = $_GET['participante_id'];
 $participante = getParticipanteById($participante_id, $conn);
 
-echo '<div>';
-echo '<h1>'.$participante['nome_completo'].'</h1>';
+if (!empty($participante)) {
+    echo '<div>';
+    echo '<h1>'.$participante[0]['nome_completo'].'</h1>';
 
-echo '<p>Personagem:'.$participante['nome_personagem'].' ('.$participante['fonte_personagem'].')</p>';
-echo '<img src="'.$participante['imagem_referencia'].'" alt="Imagem de Referência">';
-echo '</div>';
+    echo '<p>Personagem: '.$participante[0]['nome_personagem'].' ('.$participante[0]['fonte_personagem'].')</p>';
+    echo '<img src="'.$participante[0]['imagem_referencia'].'" alt="Imagem de Referência">';
+    echo '</div>';
 
-echo '<div>';
+    echo '<div>';
     echo '<form name="notas" method="POST" action="../db/notas.php?concurso_id='.$concurso_id.'&participante_id='.$participante_id.'">';
-        echo '<label for="nota">Nota:</label><br>
-        <input type="number" name="nota" min="0" max="10"><br>
+    echo '<label for="nota">Nota:</label><br>';
+    echo '<input type="number" name="nota" min="0" max="10"><br>';
 
-        <label for="observacao">Observação:</label><br>
-        <input type="text" name="observacao"><br>
+    echo '<label for="observacao">Observação:</label><br>';
+    echo '<input type="text" name="observacao"><br>';
 
-        <input type="submit" value="Confirmar" onclick="loadPage(\'iniciar_concurso.php?concurso_id=' . $concurso_id . '\')">
-    </form>
-</div>';
+    echo '<input type="submit" value="Confirmar" onclick="loadPage(\'iniciar_concurso.php?concurso_id=' . $concurso_id . '\')">';
+    echo '</form>';
+    echo '</div>';
+} else {
+    echo 'Participante não encontrado.';
+}
 ?>
